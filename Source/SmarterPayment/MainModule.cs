bbb932180186e1cab005846397724c53457adb80
Nathan Palmer <nate@thinktheweb.com>
2011-09-02 16:46:54 -0400

    Added a basic nancy app

---
+++ Source/SmarterPayment/MainModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace SmarterPayment
+{
+    public class MainModule : NancyModule 
+    {
+        public MainModule()
+        {
+            Get["/"] = (args) => View["Index"];
+        }
+    }
+}
