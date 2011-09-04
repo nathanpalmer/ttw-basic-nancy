a8104824ac1dd3a29cd6f7e9fa998be81f0f7358
Nathan Palmer <nate@thinktheweb.com>
2011-09-04 17:02:06 -0400

    removed the view engine

--- Source/SmarterPayment/MainModule.cs
+++ Source/SmarterPayment/MainModule.cs
@@ -10,7 +10,20 @@ namespace SmarterPayment
     {
         public MainModule()
         {
-            Get["/"] = (args) => View["Index"];
+            Get["/"] = (args) => @"
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+    <div>
+        Test
+    </div>
+</body>
+</html>
+";
             Get["/mu-d6235ad9-7bb860d1-37dcb55d-226fde09.txt"] = (args) => "42";
         }
     }
